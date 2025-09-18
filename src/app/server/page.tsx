61fe81bc5557010aa95d948656056a7e94902392
Aldi <aldifayol@gmail.com>
2025-09-18 17:05:14 +0800

    server component

--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -1,7 +1,18 @@
-export default function ServerPage() {
+export default async function ServerPage() {
+  const todos = await fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((res) => res.json())
+    .then((data) => data as any[]);
+
+  console.log("server component");
+
   return (
     <>
       <h1>Server Page</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
